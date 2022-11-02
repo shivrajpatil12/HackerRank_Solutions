eb15454155903f3e3246055300bbccb8a68ccd1a
shivrajtpatil12 <shivrajtpatil12@gmail.com>
2022-11-02 23:47:00 +0530

    divmod

---
+++ divmod.py
@@ -0,0 +1,14 @@
+
+
+if __name__ == '__main__':
+
+    a = int(input())
+    b = int(input())
+
+    # integer division
+    print(a//b)
+    #  module operator = remainder
+    print(a%b)
+
+    # using divmod
+    print(divmod(a,b))
